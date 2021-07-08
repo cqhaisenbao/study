ac67d62744b230b50a31b7eb28164a0b8151c3e1
cqhaisenbao <69225950+cqhaisenbao@users.noreply.github.com>
2021-07-08 20:55:24 +0800

    配置babel

--- designMode/index.js
+++ designMode/index.js
@@ -1,1 +1,12 @@
-alert(300)
+class Person {
+    constructor(name) {
+        this.name = name
+    }
+
+    getName() {
+        return this.name
+    }
+}
+
+let p = new Person('luff')
+alert(p.getName())
